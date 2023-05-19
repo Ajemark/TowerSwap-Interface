fd8e9c6ae89a36232217229d1d6524540e800bd2
Ajemark <83100392+Ajemark@users.noreply.github.com>
2023-05-19 11:29:14 +0000

    Integrating ud

--- apps/web/src/components/ConnectWalletButton.tsx
+++ apps/web/src/components/ConnectWalletButton.tsx
@@ -7,22 +7,47 @@ import useAuth from 'hooks/useAuth'
 // @ts-ignore
 // eslint-disable-next-line import/extensions
 import { useActiveHandle } from 'hooks/useEagerConnect.bmp.ts'
-import { useMemo, useState } from 'react'
+import { useMemo, useState,useEffect } from 'react'
 import { useConnect } from 'wagmi'
 import Trans from './Trans'
+import UAuth, { UserInfo } from '@uauth/js'
+import { ConnectModal } from './ConnectModal'
 
 const ConnectWalletButton = ({ children, ...props }: ButtonProps) => {
   const handleActive = useActiveHandle()
   const { login } = useAuth()
-  const {
-    t,
-    currentLanguage: { code },
-  } = useTranslation()
+  const {    t,    currentLanguage: { code },  } = useTranslation()
   const { connectAsync } = useConnect()
   const { chainId } = useActiveChainId()
   const [open, setOpen] = useState(false)
 
   const docLink = useMemo(() => getDocLink(code), [code])
+
+  const [selected, setSelected] = useState(null)
+  const [connectModalOpen, setconnectModalOpen] = useState(false)
+
+  const [uDauth, setUDauth] = useState<UAuth>()
+  const [udUser, setUdUser] = useState<UserInfo>()
+
+  useEffect(() => {
+    const uD = new UAuth({
+      clientID: "37a2f337-c4b9-465d-86df-efa58498ac20",
+      redirectUri: `${location.origin}`,
+      scope: "openid wallet email profile:optional social:optional"
+    })
+    setUDauth(uD)
+  }, [])
+
+  useEffect(async () => {
+    if(selected == 'UD' && udUser == undefined && uDauth != undefined){
+      try {
+        await uDauth.loginWithPopup()
+        location.reload()
+      } catch (error) {
+        console.log(error)
+      }
+    } else handleClick()
+  },[selected])
 
   const handleClick = () => {
     if (typeof __NEZHA_BRIDGE__ !== 'undefined') {
@@ -36,7 +61,8 @@ const ConnectWalletButton = ({ children, ...props }: ButtonProps) => {
 
   return (
     <>
-      <Button onClick={handleClick} {...props}>
+
+      <Button onClick={()=> setconnectModalOpen(true)} {...props}>
         {children || <Trans>Connect Wallet</Trans>}
       </Button>
       <WalletModalV2
@@ -47,7 +73,9 @@ const ConnectWalletButton = ({ children, ...props }: ButtonProps) => {
         login={login}
         onDismiss={() => setOpen(false)}
       />
+      <ConnectModal connectModalOpen={connectModalOpen} setSelected={setSelected} setconnectModalOpen={setconnectModalOpen} />
     </>
+    
   )
 }
 
